df8e7117b043a599e9b287d778ede6d197100e2e
LeitosDream <ipup@ipup.com>
2024-03-29 03:15:46 -0400

    Root

---
+++ build.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+set -eux;
+
+set GOARCH=amd64;
+go build -o service.bin github.com/LeitosDream/go-service/cmd;
+
+sha256sum service.bin > SHA256SUMS;
+gpg --local-user B9F4EE3E50869E93 --detach-sign --armor --output SHA256SUMS.gpg SHA256SUMS;
